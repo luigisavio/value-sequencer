29073bb204bca4fc314bd1a685797dd31839b372
luigisavio <lsavio96@gmail.com>
2026-03-11 19:54:15 +0100

    feat: sketch of parent class that defines the main sequence behavior

--- include/Sequence.hpp
+++ include/Sequence.hpp
@@ -2,80 +2,84 @@
 #define SCAN_VALUE_HPP
 
 #include <vector>
-#include <algorithm>
 
 namespace sequence
 {
-	// Generates a value that changes based on how many times a particular function is called.
-	template <typename T> // T represents the type of the value to be changed.
-	class ScanValue
+    // Abstract class to be used as parent to define a sequence
+	template <typename T, typename U> // T it the value to be changed, U defines for how much each value must be assigned
+	class Sequence
 	{
 	public:
 		static constexpr int noStepIdx{-1};
-		// Configuration of a single time step
-		struct StepPar
+
+		struct Step
 		{
-			long numberOfScans{0}; // For how many scans the value will be assigned
 			T value{};
+            U length{};
 		};
 
 		// Generic constructor
 		// Passing the vector by value. If called on an lvalue a copy is made. Otherwise move constructor is called.
 		// In both cases, the vector inside this function can be cleared by moving it to another vector for better 
 		// performances.
-		ScanValue(std::vector<StepPar> sequenceDef, const T &idleValue = {}) noexcept
-		{
+		Sequence(std::vector<Step> sequenceDef, const T &idleValue = {})
+        {
 			setSequence(std::move(sequenceDef), idleValue);
 		}
 
 		// Default constructor
-		ScanValue() noexcept {}
+		Sequence() {}
 
 		// Configure the sequence. If object was already doing something it is reset.
 		// Pass vector by value: if it is an lvalue it will copied, otherwise moved.
-		bool setSequence(std::vector<StepPar> sequenceDef, const T &idleValue = {}) noexcept;
+		bool setSequence(std::vector<Step> sequenceDef, const T &idleValue = {});
 
 		// Simply returns the value
-		const T &getValue() const noexcept;
+		const T &getValue() const;
 
 		// Updates time and returns the integral value after the update
-		const T &executeSequence() noexcept;
+		const T &executeSequence();
 
 		// Completely resets the object, equivalent to creating a new one with constructor method
-		ScanValue &reset() noexcept;
+		Sequence &reset();
 
 		// Triggers the start of the sequences
-		ScanValue &start() noexcept
+		Sequence &start()
 		{
 			m_startSequenceRequest = true;
 			return *this;
 		}
 
 		// Checks if sequence is running
-		bool isSequenceRunning() const noexcept { return (m_currentStepIndex != noStepIdx) || m_startSequenceRequest; }
+		bool isSequenceRunning() const { return (m_currentStepIndex != noStepIdx) || m_startSequenceRequest; }
+
+        virtual ~Sequence() = default;
 
 	private:
-		std::vector<StepPar> m_sequenceDef{}; // Values of the number and times during the sequence
+		std::vector<Step> m_sequenceDef{}; // Values of the number and times during the sequence
 		T m_idleValue{};
 		int m_currentStepIndex{noStepIdx}; // Current index of the step
-		long m_numOfScansAlreadyDone{0};   // Number of scans that have already been executed
-		int m_numberOfSteps{0};			   // Total number of steps to be executed
+		int m_numberOfSteps{};			   // Total number of steps to be executed, will be used in derived classes
 		bool m_startSequenceRequest{false};
 
 		// Switches to the step. Returns true if it was possible and false if the sequence is finished
-		bool switchToNextStep() noexcept;
+		bool switchToNextStep() = 0;
+        // Returns true if current step is finished
+        bool isCurrentStepFinished() = 0;
+        // Contains stuff to do to advance the step towards its length
+        void stepAdvance() = 0;
 	};
 
 	// Template functions must be defined in header file since every TU needs to see the implementation to compile the required instance
-	template <typename T>
-	const T &ScanValue<T>::getValue() const noexcept
+	template <typename T, typename U>
+	const T &Sequence<T, U>::getValue() const
 	{
 		return (m_currentStepIndex == noStepIdx) ? m_idleValue
 												 : m_sequenceDef[static_cast<std::size_t>(m_currentStepIndex)].value;
 	}
 
-	template <typename T>
-	const T &ScanValue<T>::executeSequence() noexcept
+	template <typename T, typename U>
+	const T &Sequence<T, U>::executeSequence()
 	{
 		// Check request to start the sequence
 		if (m_startSequenceRequest)
@@ -88,9 +92,11 @@ namespace sequence
 			// Check sequence is running
 			if (m_currentStepIndex != noStepIdx)
 			{
-				++m_numOfScansAlreadyDone;
+                // Advance, do it only if sequence is running for efficiency
+				stepAdvance();
+
 				// Check required number of scans have already happened
-				if (m_numOfScansAlreadyDone >= m_sequenceDef[static_cast<std::size_t>(m_currentStepIndex)].numberOfScans)
+				if (isCurrentStepFinished())
 				{
 					switchToNextStep();
 				}
@@ -99,20 +105,22 @@ namespace sequence
 		return getValue();
 	}
 
-	template <typename T>
-	bool ScanValue<T>::setSequence(std::vector<StepPar> sequenceDef,const T &idleValue) noexcept
+	template <typename T, typename U>
+	bool Sequence<T, U>::setSequence(std::vector<Step> sequenceDef,const T &idleValue)
 	{
 		reset();
-		m_sequenceDef = std::move(sequenceDef); // Avoid expensive copy by using move semantics on std vector (move assignment operator)
+        // Avoid expensive copy by using move semantics on std vector (move assignment operator)
+		m_sequenceDef = std::move(sequenceDef);
 		m_idleValue = idleValue;
-		m_numberOfSteps = static_cast<int>(m_sequenceDef.size()); // Get number of steps to be executed from size of steps vector
-		return (m_numberOfSteps > 0);
+        // Get number of steps to be executed from size of steps vector
+		m_numberOfSteps = static_cast<int>(m_sequenceDef.size());
+		return m_numberOfSteps != 0;
 	}
 
-	template <typename T>
-	ScanValue<T> &ScanValue<T>::reset() noexcept
+	template <typename T, typename U>
+	Sequence<T, U> &Sequence<T, U>::reset()
 	{
-		*this = ScanValue{};
+		*this = Sequence{};
 		return *this;
 	}
 
